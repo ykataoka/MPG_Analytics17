4ac271f4f0a27590bf4638821186a1bd33a58835
Yasu Kataoka <yk1002jp@gmail.com>
2017-03-10 12:39:21 -0800

    final version

--- Others/PriusChallenge2017/README.md
+++ Others/PriusChallenge2017/README.md
@@ -2,17 +2,17 @@
 
 ## Lap Data
 
-	#  0 timestamp				seconds		float
-	#  1 throttle				percent		float
-	#  2 brake					on/off		bool
-	#  3 gear					position	int
-	#  4 engine speed			rad/s		float
-	#  5 vehicle speed			m/s			float
-	#  6 fuel					liters		float
-	#  7 odometer				meters		float
-	#  8 latitude				degrees		float
-	#  9 longitude				degrees		float
-	# 10 not used
-	# 11 ev mode				on/off		bool
-	# 12 state of charge		percent		float
-	# 13 altitude (zCar)		meters		float
+#  0 timestamp		seconds		float
+#  1 throttle		percent		float
+#  2 brake		on/off		bool
+#  3 gear		position	int
+#  4 engine speed	rad/s		float
+#  5 vehicle speed	m/s		float
+#  6 fuel		liters		float
+#  7 odometer		meters		float
+#  8 latitude		degrees		float
+#  9 longitude		degrees		float
+# 10 not used
+# 11 ev mode		on/off		bool
+# 12 state of charge	percent		float
+# 13 altitude (zCar)	meters		float
--- Src/prius_analysis.py
+++ Src/prius_analysis.py
@@ -9,6 +9,8 @@ from sklearn.cluster import KMeans
 import matplotlib.transforms as mtrans
 from matplotlib.transforms import offset_copy
 
+from sklearn.ensemble import RandomForestRegressor
+
 mpl.rcParams['legend.fontsize'] = 10
 
 #  0 timestamp			seconds		float
@@ -55,6 +57,7 @@ def rgb(minimum, maximum, value):
     g = 255 - b - r
     return r, g, b
 
+
 def std_normalize(A):
     """
     @desc : apply standardization
@@ -62,14 +65,24 @@ def std_normalize(A):
     return (A - np.average(A)) / np.std(A)
 
 
-def show_stats(mpgs):
+def show_stats(mpgs, files):
     """
     @desc : print out average and std of mpg data
     """
+    times = []
+    for filename in files:
+        data = pd.read_json(filename)
+        data.columns = cols
+
+        # x,y data
+        t = data['time[sec]']
+        times.append(float(t.iloc[-1]))
+    print 'average time = ' + str(np.average(times))
+    print 'std time = ' + str(np.std(times))
     print 'average MPG = ' + str(np.average(mpgs))
     print 'std MPG = ' + str(np.std(mpgs))
 
-    
+
 def cluster_points(filename, N):
     """
     @desc : print out average and std of mpg data
@@ -127,7 +140,7 @@ def plot_all(files):
         b = data['brake[on/off]']
         f = data['fuel[liters]']
         c = data['state_of_charge[percent]']
-        v = data['vehiclespeed[m/s]'] * 3600. / (1000 * 1.6)
+        v = data['vehiclespeed[m/s]'] * 1.6
         z = data['altitude_zCar[meters]']
 
         # scaling
@@ -168,18 +181,62 @@ def plot_all(files):
         plt.savefig(savename, format='png', dpi=300)
         plt.clf()
 
+
+def plot_velocity(files):
+    """
+    @desc : apply clustering, find segment, find strategy
+    @param files : list of json file
+    @param centroids : fixed centroids
+    @param lapmode : in, out, fly
+    """
+    for filename in files:
+
+        # read data file
+        data = pd.read_json(filename)
+        data.columns = cols
+
+        # read meta file
+        metaname = filename.split('/')
+        metaname[2] = 'LapMeta'
+        metaname = '/'.join(metaname)
+        f = open(metaname, 'r')
+        meta = json.load(f)
+
+        # axis data
+        t = data['time[sec]']
+
+        # original data
+        f = data['fuel[liters]']
+        v = data['vehiclespeed[m/s]'] * 1.6  # v[kmh] / 1.6 = v[mph]
+#        v = data['vehiclespeed[m/s]'] * 3600. /(1000. * 1.6)  # v[kmh] / 1.6 = v[mph]
+        z = data['altitude_zCar[meters]']
+#        print data['vehiclespeed[m/s]']
+#        print v
+#        raw_input()
+
         # plot 2D velocity [mile/hour]
-        #    title = 'VELOCITY : mpg = ' + str(meta['mpg']) + ', param = ' + str(meta['name'])
-        #    plt.title(title)
-        #    plt.xlabel('Time[sec]')
-        #    plt.ylabel('Velocity[mile/hour]')
-        #    # plt.xlim(0,0.01)
-        #    plt.ylim(0, 100)
-        #    plt.grid(True)
-        #    plt.plot(t, v)
-        #    plt.plot(t, e*80.)
-        #    plt.plot(t, c*100.)
-        #    plt.show()
+        title = 'VELOCITY : mpg = ' + str(meta['mpg']) + ', param = ' + str(meta['name'])
+        plt.title(title)
+        plt.xlabel('Time[sec]')
+        plt.ylabel('Altitude[m] / Velocity[mile/h]')
+        # plt.xlim(0,0.01)
+        plt.ylim(-5, 100)
+        plt.grid(True)
+#        plt.plot(t, v, label='converted v_conv[mile/h] = v_org[m/s] * 3600. / (1000. * 1.6)')
+        plt.plot(t, v, label='velocity[mile/h]')
+        plt.plot(t, z, label='altitude')
+        plt.legend()
+
+        if meta['name'].find('Out') == 0:
+            savename = 'result/OutLap/velocity_' + filename.split('/')[-1] + '.png'
+        elif meta['name'].find('Fly') == 0:
+            savename = 'result/FlyLap/velocity_' + filename.split('/')[-1] + '.png'
+        elif meta['name'].find('In') == 0:
+            savename = 'result/InLap/velocity_' + filename.split('/')[-1] + '.png'
+        plt.savefig(savename, format='png', dpi=300)
+        plt.clf()
+        
+#        plt.show()
 
 
 def show_3D_plot(centroids):
@@ -245,6 +302,7 @@ def ev_mode_analytics(datas, centroids, num_c, lapmode, datamode):
     plt.grid(True)
     savename = datamode + '_ev_' + lapmode + '.png'
     plt.savefig(savename, format='png', dpi=300)
+    plt.clf()
     #plt.show()
 
 
@@ -291,6 +349,7 @@ def brake_analytics(datas, centroids, num_c, lapmode, datamode):
     plt.grid(True)
     savename = datamode + '_brake_' + lapmode + '.png'
     plt.savefig(savename, format='png', dpi=300)
+    plt.clf()
 #    plt.show()
 
 
@@ -337,6 +396,7 @@ def gear_analytics(datas, centroids, num_c, lapmode, datamode):
     plt.grid(True)
     savename = datamode + '_gear_' + lapmode + '.png'
     plt.savefig(savename, format='png', dpi=300)
+    plt.clf()
 #    plt.show()
 
 
@@ -384,8 +444,125 @@ def throttle_analytics(datas, centroids, num_c, lapmode, datamode):
     plt.grid(True)
     savename = datamode + '_thtl_' + lapmode + '.png'
     plt.savefig(savename, format='png', dpi=300)
+    plt.clf()
 #    plt.show()
 
+
+def feature_generation(datas, centroids, num_c):
+    """
+    @desc : analysis and visualization on ev switching
+    @param datas : data enhanced by cluster label
+    @param centroids : fixed centroids
+    @param num_c : number of cluster
+    """
+    # output feature
+    E, B, G, T, X = [], [], [], [], []
+
+    # E(ev)        = [data_num x 150] matrix
+    # B(brake)     = [data_num x 150] matrix
+    # G(gear)      = [data_num x 150] matrix
+    # T(throttle)  = [data_num x 150] matrix
+    # X =[E,B,G,T] = [data_num x 600] matrix
+
+    # loop for each cluster
+    # EV mode [1:EV ON, 0:EV OFF]
+    for c in range(num_c):
+        tmp_ev_ave = []
+        for data in datas:  # loop for each cluster
+            ev_l = data[data['label'] == c]['evmode[on/off]']
+            ev_l_ave = np.average(ev_l)  # take average at each cluster
+            tmp_ev_ave.append(ev_l_ave)
+        E.append(tmp_ev_ave)
+    E = np.array(E).transpose()  # 9 x 150 matrix when 'high'
+    E_f_name = ['E_' + str(i) for i in range(150)]
+
+    # Brake mode [1:Brake ON, 0:Brake OFF]
+    for c in range(num_c):
+        tmp_brake_ave = []
+        for data in datas:  # loop for each cluster
+            brake_l = data[data['label'] == c]['brake[on/off]']
+            brake_l_ave = np.average(brake_l)
+            tmp_brake_ave.append(brake_l_ave)
+        B.append(tmp_brake_ave)
+    B = np.array(B).transpose()  # 9 x 150 matrix when 'high'
+    B_f_name = ['B_' + str(i) for i in range(150)]
+
+    # Gear mode [1:Drive, 0:Neutral]
+    for c in range(num_c):
+        tmp_gear_ave = []
+        for data in datas:  # loop for each cluster
+            gear_l = data[data['label'] == c]['gear[position]']
+            gear_l_ave = np.average(gear_l - 2)  # scaling
+            tmp_gear_ave.append(gear_l_ave)
+        G.append(tmp_gear_ave)
+    G = np.array(G).transpose()  # 9 x 150 matrix when 'high'
+    G_f_name = ['G_' + str(i) for i in range(150)]
+
+    # Throttle mode [1:100%, 0:0%]
+    for c in range(num_c):
+        tmp_thtl_ave = []
+        for data in datas:  # loop for each cluster
+            thtl_l = data[data['label'] == c]['throttle[percent]']
+            thtl_l_ave = np.average(thtl_l)  # subtract offset position
+            tmp_thtl_ave.append(thtl_l_ave)
+        T.append(tmp_thtl_ave)
+    T = np.array(T).transpose()  # 9 x 150 matrix when 'high'
+    T_f_name = ['T_' + str(i) for i in range(150)]
+
+    X = np.concatenate([E, B, G, T], axis=1)
+    feature_name = E_f_name + B_f_name + G_f_name + T_f_name
+    print feature_name
+
+    return [X, feature_name]
+
+
+def label_creation(files):
+    """
+    @desc create label data, list of mpg
+    """
+    Y = []  # list of mpg
+    for filename in files:
+        # read meta file
+        metaname = filename.split('/')
+        metaname[2] = 'LapMeta'
+        metaname = '/'.join(metaname)
+        f = open(metaname, 'r')
+        meta = json.load(f)
+        Y.append(meta['mpg'])
+
+    return Y
+
+
+def supervised_learning(X, Y):
+    """
+    @desc apply RandomForest and output Feature Importance
+    """
+    rf = RandomForestRegressor()
+    rf.fit(X, Y)
+    pred_Y = rf.predict(X)
+    print 'predicted mpg = ', pred_Y
+    print '     true mpg = ', Y
+    print 'MSE = ', np.average(abs(pred_Y - Y))
+    print 'R^2 score = ', rf.score(X, Y)
+
+    return rf.feature_importances_
+
+
+def show_feature_importance(feature_name, Imp):
+    """
+    @desc List up feature_name based on ranking
+    """
+    ranking = sorted(zip(map(lambda x: round(x, 4), Imp), feature_name),
+                     reverse=True)
+
+    topK = 20
+    table = []
+    for i in range(topK):
+        tmp = [ranking[i][0], ranking[i][1]]
+        table.append(tmp)
+
+    print pd.DataFrame(table, columns=['Importance', 'Feature_name'])
+    
 
 def cluster_analysis(files, centroids, lapmode, datamode):
     """
@@ -398,7 +575,10 @@ def cluster_analysis(files, centroids, lapmode, datamode):
     datas = []  # list of pandas data
     num_c = len(centroids)
 
-    # clustering : identify the cluster id for each file's point
+    """
+    clustering : identify the cluster id for each file's point
+    @output datas has the cluster id(label) column
+    """
     for filename in files:
 
         # read data file
@@ -425,17 +605,45 @@ def cluster_analysis(files, centroids, lapmode, datamode):
         data = pd.concat([data, tmp], axis=1)  # add to the data
         datas.append(data)
 
-    # ev_mode
-    ev_mode_analytics(datas, centroids, num_c, lapmode, datamode)
+    """
+    Plot Altitude at Each Cluster ID
+    """
+    # if datamode == 'high':
+    #     filename = files[1].split('/')[-1]
+    #     tmp = datas[1]
+    #     z = tmp['altitude_zCar[meters]']
+    #     v = tmp['vehiclespeed[m/s]'] * 1.6  # !this is trick for true mph
+    #     t = tmp['label']
+    #     title = 'altitude vs label ID'
+    #     plt.title(title)
+    #     plt.xlabel('Label ID')
+    #     plt.ylabel('Altitude [m] / vehicle speed[mile/h]')
+    #     # plt.xlim(0,0.01)
+    #     plt.ylim(-5, 100)
+    #     plt.plot(t, z, label='altitude')
+    #     plt.plot(t, v, label='converted v[mile/h]')
+    #     plt.grid(True)
+    #     plt.legend()
+    #     savename = lapmode + '_altitude_labelID_' + filename + '.png'
+    #     plt.savefig(savename, format='png', dpi=300)
+    #     plt.clf()
 
-    # brake
-    brake_analytics(datas, centroids, num_c, lapmode, datamode)
+    """
+    Behavior Analytics at Each Cluster ID
+    """
+    #     ev_mode_analytics(datas, centroids, num_c, lapmode, datamode)
+    #     brake_analytics(datas, centroids, num_c, lapmode, datamode)
+    #     gear_analytics(datas, centroids, num_c, lapmode, datamode)
+    #     throttle_analytics(datas, centroids, num_c, lapmode, datamode)
 
-    # gear
-    gear_analytics(datas, centroids, num_c, lapmode, datamode)
+    """
+    Strategy Importance Analysis
+    """
+    X, feature_name = feature_generation(datas, centroids, num_c)
+    Y = label_creation(files)
+    Imp = supervised_learning(X, Y)  # Imp = feature_importances_
+    show_feature_importance(feature_name, Imp)
 
-    # throttle
-    throttle_analytics(datas, centroids, num_c, lapmode, datamode)
 
 # variables
 OutLapFiles = []
@@ -509,37 +717,40 @@ for filename in org_files:
 # show_3D_plot(fly_centroids)
 # show_3D_plot(in_centroids)
 
+# plot velocity
+# plot_velocity(HighOutLapFiles)
+# plot_velocity(HighFlyLapFiles)
+# plot_velocity(HighInLapFiles)
+
 # deal with OutLap
 print "outlap data analytics"
-show_stats(OutMPG)
+show_stats(OutMPG, OutLapFiles)
 print 'all out : ', OutLapFiles
 cluster_analysis(OutLapFiles, out_centroids, 'out', 'all')
-print 'high out : ', HighOutLapFiles
-cluster_analysis(HighOutLapFiles, out_centroids, 'out', 'high')
-print 'low out : ', LowOutLapFiles
-cluster_analysis(LowOutLapFiles, out_centroids, 'out', 'low')
+# print 'high out : ', HighOutLapFiles
+# cluster_analysis(HighOutLapFiles, out_centroids, 'out', 'high')
+# print 'low out : ', LowOutLapFiles
+# cluster_analysis(LowOutLapFiles, out_centroids, 'out', 'low')
 # plot_all(OutLapFiles)
-
 
 # deal with FlyLap
 print "flylap data analytics"
-show_stats(FlyMPG)
+show_stats(FlyMPG, FlyLapFiles)
 print 'all fly : ', FlyLapFiles
 cluster_analysis(FlyLapFiles, fly_centroids, 'fly', 'all')
-print 'high fly : ', HighFlyLapFiles
-cluster_analysis(HighFlyLapFiles, fly_centroids, 'fly', 'high')
-print 'low fly : ', LowFlyLapFiles
-cluster_analysis(LowFlyLapFiles, fly_centroids, 'fly', 'low')
+# print 'high fly : ', HighFlyLapFiles
+# cluster_analysis(HighFlyLapFiles, fly_centroids, 'fly', 'high')
+# print 'low fly : ', LowFlyLapFiles
+# cluster_analysis(LowFlyLapFiles, fly_centroids, 'fly', 'low')
 # plot_all(FlyLapFiles)
-
 
 # deal with InLap
 print "inlap data analytics"
-show_stats(InMPG)
+show_stats(InMPG, InLapFiles)
 print 'all in : ', InLapFiles
 cluster_analysis(InLapFiles, in_centroids, 'in', 'all')
-print 'high in : ', HighInLapFiles
-cluster_analysis(HighInLapFiles, in_centroids, 'in', 'high')
-print 'low in : ', LowInLapFiles
-cluster_analysis(LowInLapFiles, in_centroids, 'in', 'low')
+# print 'high in : ', HighInLapFiles
+# cluster_analysis(HighInLapFiles, in_centroids, 'in', 'high')
+# print 'low in : ', LowInLapFiles
+# cluster_analysis(LowInLapFiles, in_centroids, 'in', 'low')
 # plot_all(InLapFiles)
